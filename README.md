dc4e95caad1576eabc3051d269b608e6e04dc628
Jana Ivonne Franke <jana.ivonne.franke@hs-furtwangen.de>
2021-11-21 10:30:30 +0100

    Aufgabe Dom und Events
    änderung html und erstellung index.ts

--- README.md
+++ README.md
@@ -20,3 +20,11 @@
 
  * Code: https://github.com/CleoKaty/GIS-WS-21-22-Abgabe/tree/main/Typescript/Aufgabe1
  * GitHub Pages: https://cleokaty.github.io/GIS-WS-21-22-Abgabe/Typescript/Aufgabe1/index.html
+
+ </br>
+
+ ### **4. Typescript-DOM und Events**
+
+ * Code: https://github.com/CleoKaty/GIS-WS-21-22-Abgabe/tree/main/Typescript/Aufgabe2
+ * GitHub Pages: https://cleokaty.github.io/GIS-WS-21-22-Abgabe/Typescript/Aufgabe2/events.html
+
--- Typescript/Aufgabe2/index.js
+++ Typescript/Aufgabe2/index.js
@@ -1,24 +1,23 @@
-namespace Aufgabe {
+"use strict";
+var Aufgabe;
+(function (Aufgabe) {
     /*Kill me pls */
-    let interpret: HTMLInputElement = <HTMLInputElement> document.getElementById("interpret") as HTMLInputElement;
-    let preis: HTMLInputElement = <HTMLInputElement> document.getElementById("preis") as HTMLInputElement;
-    let datum: HTMLInputElement = <HTMLInputElement> document.getElementById("zeit") as HTMLInputElement;
-    let button: HTMLElement = document.getElementById("enter");
-    
-    
+    let interpret = document.getElementById("interpret");
+    let preis = document.getElementById("preis");
+    let datum = document.getElementById("zeit");
+    let button = document.getElementById("enter");
     console.log(interpret);
-
-    button.addEventListener("click", () => { 
+    button.addEventListener("click", () => {
         if (interpret.value != "" && preis.value != "" && datum.value != "") {
             interpret.style.borderColor = "black";
             preis.style.borderColor = "black";
             datum.style.borderColor = "black";
-            let liste: HTMLElement = document.createElement("tr");
-            let a: HTMLElement = document.createElement("td");
-            let b: HTMLElement = document.createElement("td");
-            let c: HTMLElement = document.createElement("td");
-            let d: HTMLElement = document.createElement("td");
-            let deletebutton: HTMLElement = document.createElement("button");
+            let liste = document.createElement("tr");
+            let a = document.createElement("td");
+            let b = document.createElement("td");
+            let c = document.createElement("td");
+            let d = document.createElement("td");
+            let deletebutton = document.createElement("button");
             deletebutton.innerText = "delete";
             document.getElementById("table").appendChild(liste);
             a.innerText = interpret.value;
@@ -30,12 +29,11 @@ namespace Aufgabe {
             liste.appendChild(c);
             liste.appendChild(d);
             deletebutton.addEventListener("click", deleter);
-
-            function deleter (): void {
-            document.getElementById("table").removeChild(liste);
+            function deleter() {
+                document.getElementById("table").removeChild(liste);
             }
         }
-        else { 
+        else {
             if (interpret.value == "") {
                 interpret.style.borderColor = "red";
             }
@@ -46,7 +44,6 @@ namespace Aufgabe {
                 datum.style.borderColor = "red";
             }
         }
-       
     });
-    
-}
+})(Aufgabe || (Aufgabe = {}));
+//# sourceMappingURL=index.js.map
